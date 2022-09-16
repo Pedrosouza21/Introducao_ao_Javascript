15b6576233a7a28c878b9023049a9b4420e57119
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-16 00:16:57 -0300

    learning functions

---
+++ index.js
@@ -0,0 +1,22 @@
+/* function 
+
+ function somar (x1, x2) {
+  
+    return x1 + x2;
+ }
+
+  let resultado = somar(1,2);
+  console.log(resultado);
+  
+*/
+
+/* function Calc 
+function calc(x1, x2) {
+ 
+   return eval `${x1} ${operator} ${x2}`;
+}
+
+ let resultado = somar(1, 2, "+");
+ console.log(resultado);
+
+*/
