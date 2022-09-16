1a25d75ed1e4f858e3ed5f98ee5faf6f4121a5e6
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-09-16 00:25:56 -0300

    inserting events

--- index.js
+++ index.js
@@ -20,3 +20,39 @@ function calc(x1, x2) {
  console.log(resultado);
 
 */
+
+/* function Anonima
+  (function calc(x1, x2,operator) {
+  
+    return eval `${x1} ${operator} ${x2}`;
+
+ })(1,2, "*");
+
+ let resultado = somar(1, 2, "+");
+  console.log(resultado);
+
+*/
+
+
+/* function 
+ let calc(x1, x2,operator) =>  {
+    return eval `${x1} ${operator} ${x2}`;
+
+ })(1,2, "*");
+
+ let resultado = somar(1, 2, "+");
+  console.log(resultado);
+*/
+
+
+/*Eventos */
+
+window.addEventListener('focus', event => {
+   console.log("focus");
+
+});
+
+document.addEventListener('click', event => {
+   console.log('click');
+
+});
